8221357c899b577fc069aacd4892b7fe7aa745f4
stackinspector <stackinspector@126.com>
2021-09-17 19:38:10 +0800

    recorder with storage that works

--- src/bin/livekit.rs
+++ src/bin/livekit.rs
@@ -1,23 +1,27 @@
-use tokio::spawn;
+use tokio::{spawn, signal};
 use tokio::time::{self, Duration};
 use futures_util::{StreamExt, SinkExt};
 use tokio_tungstenite::{connect_async, tungstenite::protocol::Message};
 use structopt::StructOpt;
-use livekit::{package::Package, connect::Connect};
+use rocksdb::DB;
+use livekit::{package::Package, connect::Connect, util::Timestamp};
 
 #[derive(StructOpt)]
 struct Args {
     #[structopt(short = "r", long)]
     roomid: u32,
+    #[structopt(short = "p", long)]
+    storage_path: String,
 }
 
 #[tokio::main]
 async fn main() {
-    let Args { roomid } = Args::from_args();
-    let connection = Connect::new(roomid).await.unwrap();
+    let args = Args::from_args();
+    let connection = Connect::new(args.roomid).await.unwrap();
     let init = Message::Binary(Package::create_init_request(&connection).encode());
     let heartbeat = Message::Binary(Package::HeartbeatRequest().encode());
 
+    let storage = DB::open_default(args.storage_path).unwrap();
     let (socket, _) = connect_async(connection.url.as_str()).await.unwrap();
     let (mut write, read) = socket.split();
 
@@ -31,12 +35,22 @@ async fn main() {
         }
     });
 
-    read.for_each(|message| async {
-        match message.unwrap() {
-            Message::Binary(payload) => println!("{:?}", Package::decode(&payload)),
-            _ => panic!("unexpected websocket message type"),
-        }
-    }).await;
+    spawn(async move {
+        read.for_each(|message| async {
+            match message.unwrap() {
+                Message::Binary(payload) => {
+                    let package = Package::decode(&payload);
+                    println!("{:?}", package);
+                    storage.put(Timestamp::now().to_bytes(), payload).unwrap();
+                },
+                _ => panic!("unexpected received websocket message type"),
+            }
+        }).await
+    });
+
+    signal::ctrl_c().await.unwrap();
+
+    println!("quit");
 
     // socket.close(None);
 }
