c5ecbe5e1f2b4700c35a737ee48a94982a62d576
stackinspector <stackinspector@126.com>
2021-09-19 09:18:53 +0800

    heartbeat_rate

--- src/bin/livekit.rs
+++ src/bin/livekit.rs
@@ -12,23 +12,26 @@ struct Args {
     roomid: u32,
     #[structopt(short = "p", long)]
     storage_path: String,
+    #[structopt(long, default_value = "30")]
+    heartbeat_rate: u64,
 }
 
 #[tokio::main]
 async fn main() {
-    let args = Args::from_args();
-    let connection = Connect::new(args.roomid).await.unwrap();
-    let init = Message::Binary(Package::create_init_request(&connection).encode());
-    let heartbeat = Message::Binary(Package::HeartbeatRequest().encode());
+    let Args { roomid, storage_path, heartbeat_rate } = Args::from_args();
+    assert!(heartbeat_rate < 60 && heartbeat_rate >= 1);
 
-    let storage = DB::open_default(args.storage_path).unwrap();
+    let connection = Connect::new(roomid).await.unwrap();
+    let storage = DB::open_default(storage_path).unwrap();
     let (socket, _) = connect_async(connection.url.as_str()).await.unwrap();
     let (mut write, read) = socket.split();
 
+    let init = Message::Binary(Package::create_init_request(&connection).encode());
     write.send(init).await.unwrap();
 
     spawn(async move {
-        let mut interval = time::interval(Duration::from_secs(30));
+        let heartbeat = Message::Binary(Package::HeartbeatRequest().encode());
+        let mut interval = time::interval(Duration::from_secs(heartbeat_rate));
         loop {
             interval.tick().await;
             write.send(heartbeat.clone()).await.unwrap();
@@ -36,14 +39,20 @@ async fn main() {
     });
 
     spawn(async move {
-        read.for_each(|message| async {
-            match message.unwrap() {
-                Message::Binary(payload) => {
-                    let package = Package::decode(&payload);
-                    println!("{:?}", package);
-                    storage.put(Timestamp::now().to_bytes(), payload).unwrap();
+        read.for_each(|maybe_message| async {
+            match maybe_message {
+                Ok(message) => match message {
+                    Message::Binary(payload) => {
+                        let package = Package::decode(&payload);
+                        println!("{:?}", package);
+                        storage.put(Timestamp::now().to_bytes(), payload).unwrap();
+                    },
+                    any_other => {
+                        println!("{:?}", any_other);
+                        panic!("unexpected received websocket message type");
+                    },
                 },
-                _ => panic!("unexpected received websocket message type"),
+                Err(error) => panic!("{}", error),
             }
         }).await
     });
