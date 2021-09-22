6e5b0aba59dfbce22f5f6628cf867ab0dc34e1d2
stackinspector <stackinspector@126.com>
2021-09-22 11:02:51 +0800

    adjust

--- src/config.rs
+++ src/config.rs
@@ -1,9 +1,9 @@
 use serde::{Deserialize, Serialize};
 
 pub const VERSION: &str = concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"), "-alpha-ms2");
-pub const STORAGE_VERSION: &str = "alpha1";
+pub const STORAGE_VERSION: &str = "alpha2";
 
-pub const EVENT_CHANNEL_BUFFER_SIZE: usize = 32;
+pub const EVENT_CHANNEL_BUFFER_SIZE: usize = 1024;
 pub const HEARTBEAT_RATE_SEC: u64 = 30;
 
 #[derive(Serialize, Deserialize)]
