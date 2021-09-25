c156fab92f411e6e6d3f352d1ace114b63760e50
stackinspector <stackinspector@126.com>
2021-09-26 01:37:30 +0800

    file name template

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,4 @@
-use serde::{Deserialize, Serialize};
+use serde::{Serialize, Deserialize};
 
 pub const VERSION: &str = concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"), "-alpha-ms2");
 pub const STORAGE_VERSION: &str = "alpha2";
@@ -6,6 +6,8 @@ pub const STORAGE_VERSION: &str = "alpha2";
 pub const EVENT_CHANNEL_BUFFER_SIZE: usize = 1024;
 pub const HEARTBEAT_RATE_SEC: u64 = 30;
 pub const RETRY_INTERVAL_SEC: u64 = 30;
+
+pub const DEFAULT_FILE_TEMPLATE: &str = "{roomid}-{date}-{time}{ms}-{title}";
 
 #[derive(Serialize, Deserialize)]
 pub struct General {
@@ -25,11 +27,17 @@ pub struct Group {
 }
 
 #[derive(Serialize, Deserialize)]
+pub struct GroupRecordConfig {
+    pub quality: Option<Vec<i32>>,
+    pub file_root: String,
+    pub file_template: Option<String>,
+}
+
+#[derive(Serialize, Deserialize)]
 pub struct GroupConfig {
     pub storage_root: String,
-    pub record_root: String,
     pub access_token: Option<String>,
-    pub record_quality: Option<Vec<i32>>,
+    pub record: GroupRecordConfig,
 }
 
 #[derive(Serialize, Deserialize)]
--- src/room.rs
+++ src/room.rs
@@ -1,6 +1,10 @@
+use rand::{Rng, thread_rng as rng};
 use rocksdb::DB;
 use crate::{
-    config::{STORAGE_VERSION, EVENT_CHANNEL_BUFFER_SIZE, RoomConfig, GroupConfig},
+    config::{
+        STORAGE_VERSION, EVENT_CHANNEL_BUFFER_SIZE, DEFAULT_FILE_TEMPLATE,
+        RoomConfig, GroupConfig,
+    },
     rest::room::RoomInfo,
     client::{channel, Sender, Receiver},
 };
@@ -54,5 +58,23 @@ impl Room {
 
     pub fn receiver(&self) -> Receiver {
         self.channel_sender.subscribe()
+    }
+
+    pub fn record_file_name(&self, file_template: Option<String>) -> String {
+        // group_config.record.file_template.clone()
+        let template = file_template.unwrap_or_else(|| DEFAULT_FILE_TEMPLATE.to_string());
+        let time = chrono::Utc::now();
+
+        template
+            .replace("{date}", time.format("%y%M%d").to_string().as_str())
+            .replace("{time}", time.format("%H%m%s").to_string().as_str())
+            .replace("{ms}", time.format("%.3f").to_string().as_str())
+            .replace("{ts}", time.timestamp_millis().to_string().as_str())
+            .replace("{random}", rng().gen_range(0..100).to_string().as_str())
+            .replace("{roomid}", self.roomid.to_string().as_str())
+            .replace("{title}", self.info.title.as_str())
+            // .replace("{name}", self.name)
+            .replace("{parea}", self.info.parent_area_name.as_str())
+            .replace("{area}", self.info.area_name.as_str())
     }
 }
