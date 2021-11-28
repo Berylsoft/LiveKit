188ea87fcb7833aaae0605bcace0247745eaa0a9
stackinspector <stackinspector@126.com>
2021-11-28 21:12:30 +0800

    fix

--- feed/lib.rs
+++ feed/lib.rs
@@ -1,4 +1,11 @@
-pub mod config;
+pub mod config {
+    pub const FEED_HEARTBEAT_RATE_SEC: u64 = 30;
+    pub const FEED_INIT_INTERVAL_MILLISEC: u64 = 100;
+    pub const FEED_RETRY_INTERVAL_MILLISEC: u64 = 5000;
+    pub const FEED_INIT_RETRY_INTERVAL_SEC: u64 = 5000;
+    pub const FEED_TCP_BUFFER_SIZE: usize = 4096;
+}
+
 pub mod util;
 #[cfg(feature = "package")]
 pub mod package;
