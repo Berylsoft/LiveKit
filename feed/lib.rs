70748209690ab07747379f50f3009fb84f3ab389
stackinspector <stackinspector@126.com>
2021-12-31 23:23:28 +0800

    fix const

--- feed/lib.rs
+++ feed/lib.rs
@@ -2,8 +2,8 @@ pub mod config {
     pub const FEED_HEARTBEAT_RATE_SEC: u64 = 30;
     pub const FEED_INIT_INTERVAL_MS: u64 = 100;
     pub const FEED_RETRY_INTERVAL_MS: u64 = 5000;
-    pub const FEED_INIT_RETRY_INTERVAL_SEC: u64 = 5000;
-    pub const FEED_TCP_BUFFER_SIZE: usize = 4096;
+    pub const FEED_INIT_RETRY_INTERVAL_SEC: u64 = 5;
+    pub const FEED_TCP_BUFFER_SIZE: usize = 1024 * 8;
     pub const FEED_STORAGE_CACHE_MAX_BYTE: u64 = 1024 * 1024 * 16;
     pub const FEED_STORAGE_FLUSH_INTERVAL_MS: u64 = 1000;
 }
