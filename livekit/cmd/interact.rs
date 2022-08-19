147893208a03de790c95a50b7b202d1a35df80a1
stackinspector <stackinspector@126.com>
2022-08-19 21:27:23 +0800

    livekit/cmd/interact: subcommand

--- livekit/cmd/interact.rs
+++ livekit/cmd/interact.rs
@@ -1,5 +1,4 @@
 use std::{path::PathBuf, fs};
-use serde::Deserialize;
 use bilibili_restapi_live::interact as api;
 use bilibili_restapi_client::{client::Client, access::Access};
 use livekit_feed::stream::now;
@@ -11,23 +10,39 @@ pub struct Args {
     /// json access file path of bilibili_restapi
     #[argh(option, short = 'a')]
     access_path: PathBuf,
-    /// json request
-    #[argh(option, short = 'r')]
-    request: String,
+    #[argh(subcommand)]
+    request: Request,
 }
 
-#[derive(Deserialize)]
-#[serde(tag = "type", content = "data")]
+#[derive(argh::FromArgs)]
+#[argh(subcommand)]
 enum Request {
-    Danmaku { roomid: u32, msg: String, rnd: Option<i64>, emoji: bool },
+    Danmaku(Danmaku),
+}
+
+/// send a common danmaku to specified room
+#[derive(argh::FromArgs)]
+#[argh(subcommand, name = "send-dm")]
+struct Danmaku {
+    /// target roomid (no short id)
+    #[argh(option, short = 'r')]
+    roomid: u32,
+    /// danmaku content
+    #[argh(option, short = 't')]
+    text: String,
+    /// customize the rand value
+    #[argh(option)]
+    rand: Option<i64>,
+    /// emoji danmaku
+    #[argh(switch)]
+    emoji: bool,
 }
 
 pub async fn main(Args { access_path, request }: Args) {
     let access: Access = serde_json::from_reader(fs::OpenOptions::new().read(true).open(access_path).unwrap()).unwrap();
-    let payload: Request = serde_json::from_str(&request).unwrap();
     let client = Client::new(Some(access), None);
-    match payload {
-        Request::Danmaku { roomid, msg, rnd, emoji } => {
+    match request {
+        Request::Danmaku(Danmaku { roomid, text: msg, rand: rnd, emoji }) => {
             let rnd = rnd.unwrap_or_else(|| now().try_into().unwrap());
             let send = api::SendDanmaku::new(roomid, msg, rnd, emoji);
             println!("{:?}", client.call(&send).await);
